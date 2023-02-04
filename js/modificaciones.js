f9caec6b0c6819b6317443facc648a2f858941f2
Gonzalop0210 <gonzalop0210@gmail.com>
2023-02-03 21:15:25 -0500

    Vegetales terminado

---
+++ js/modificaciones.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < vegetalesArray.length; i++) {
+    if (vegetalesArray[i].nombre == 'Ají amarillo molido') {
+        
+    };
+};
--- js/product-list.js
+++ js/product-list.js
@@ -1,7 +1,97 @@
 const vegetalesArray = [];
 
 vegetalesArray.push({
-    img: '../img/Abarrotes/aceiteDeleite.jpg',
-    nombre: 'Aceite Vegetal Deleite',
-    precio: 11.50,
+    img: '../img/Vegetales/ajiAmarilloEntero.jpg',
+    nombre: 'Ají amarillo entero',
+    precio: 0.6,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/ajiAmarilloMolido.jpg',
+    nombre: 'Ají amarillo molido',
+    precio: 0.5,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/arverja.jpg',
+    nombre: 'Arverja entera',
+    precio: 16.00,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/camoteAmarillo.jpg',
+    nombre: 'Camote amarillo',
+    precio: 3.0,
+});
+
+vegetalesArray.push({
+    img:'../img/Vegetales/camoteMorado.jpg',
+    nombre: 'Camote Morado',
+    precio: 2.50,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/cebollaRoja.jpg',
+    nombre: 'Cebolla roja',
+    precio: 2.50,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/limon.jpg',
+    nombre: 'Limón',
+    precio: 8.0,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/papaAmarilla.jpg',
+    nombre: 'Papa amarilla',
+    precio: 6.0,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/papaCanchan.jpg',
+    nombre: 'Papa canchan',
+    precio: 3.0,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/papaHuayro.jpg',
+    nombre: 'Papa Huayro',
+    precio: 5.0,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/papaYungay.jpg',
+    nombre: 'Papa yungay',
+    precio: 3.0,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/pimienton.jpg',
+    nombre: 'Pimiento rojo',
+    precio: '0.5',
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/rocotoEntero.jpg',
+    nombre: 'Rocoto entero',
+    precio: 0.5,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/tomate.jpg',
+    nombre: 'Tomate',
+    precio: 2.80,
+});
+
+vegetalesArray.push({
+    img: '../img/Vegetales/zanahoria.jpg',
+    nombre: 'Zanahoria',
+    precio: 0.6,
+});
+
+vegetalesArray.push({
+    img:'../img/Vegetales/zapallo.jpg',
+    nombre: 'Zapallo',
+    precio: 0.5,
 });
